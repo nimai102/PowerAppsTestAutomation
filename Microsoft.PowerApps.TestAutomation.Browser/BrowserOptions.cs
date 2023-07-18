75976479eaf41ad633b5c16daae115d83958e68a
nimai102 <110051007+nimai102@users.noreply.github.com>
2023-07-18 16:56:38 +0200

    Update BrowserOptions.cs

--- Microsoft.PowerApps.TestAutomation.Browser/BrowserOptions.cs
+++ Microsoft.PowerApps.TestAutomation.Browser/BrowserOptions.cs
@@ -87,11 +87,11 @@ namespace Microsoft.PowerApps.TestAutomation.Browser
         public virtual EdgeOptions ToEdge()
         {
             var options = new EdgeOptions();
-            
+            options.AddArgument("--remote-debugging-port=9222");
             {
                 PageLoadStrategy = PageLoadStrategy.Normal
             };
-            options.AddArgument("--remote-debugging-port=9222");
+            
             return options;
         }
     }
