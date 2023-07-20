5a4f3448470f9338b1e369dae4bbc3be448ae924
nimai102 <110051007+nimai102@users.noreply.github.com>
2023-07-20 16:44:14 +0200

    Update TestAutomation.cs

--- Microsoft.PowerApps.TestAutomation.Api/Pages/TestAutomation.cs
+++ Microsoft.PowerApps.TestAutomation.Api/Pages/TestAutomation.cs
@@ -114,7 +114,7 @@ namespace Microsoft.PowerApps.TestAutomation.Api
                
                 foreach (var b in buttons)
                 {    
-                    Console.WriteLine($"b.Text");
+                    Console.WriteLine($"ButtonText: {b.Text}");
                     Console.WriteLine("Suche Sign in");
                     if (b.Text.Equals("Sign In"))
                     {
