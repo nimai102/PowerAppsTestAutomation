6255f19e780280e0dbdfb37f5ec240394f772c2f
nimai102 <110051007+nimai102@users.noreply.github.com>
2023-07-21 14:16:03 +0200

    Update TestAutomation.cs

--- Microsoft.PowerApps.TestAutomation.Api/Pages/TestAutomation.cs
+++ Microsoft.PowerApps.TestAutomation.Api/Pages/TestAutomation.cs
@@ -34,10 +34,10 @@ namespace Microsoft.PowerApps.TestAutomation.Api
             {
                 // Navigate to TestSuite or TestCase URL
                 InitiateTest(driver, uri);
-
+                Console.WriteLine("Test initialisiert");
                 // Check for existence of permissions dialog (1st test load for user)
                 CheckForPermissionDialog(driver);
-
+                Console.WriteLine("Permission gecheckt");
                 // Try to report the sessionId. There is a bit of a race condition here,
                 // so don't do this too close to fullscreen-app-host visibility or it 
                 // will fail to find Core or some other namespace.
